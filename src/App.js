b040f28fb2826f735b4a7391dce225555544f9af
Manoj Karnati <manoj.karnati@seygeninc.com>
2020-09-23 20:12:52 -0400

    added getTNList method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment, useLayoutEffect } from "react";
+import "./App.css";
+import sampleData from "./xmls/items_list.js";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getTNList();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getTNList() {
+    const domParser = new DOMParser();
+    const doc = domParser.parseFromString(sampleData, "application/xml");
+    const telephonesList = doc.getElementsByTagName("TelephoneNumberId");
+    let tnUL = document.createElement("ul");
+    let tns = [];
+    for (let i = 0; i < telephonesList.length; i++) {
+      tns.push(telephonesList[i].firstChild.nodeValue);
+      let li = document.createElement("li");
+      li.textContent = telephonesList[i].firstChild.nodeValue;
+      tnUL.appendChild(li);
+      console.log(tnUL);
+    }
+
+    return tnUL.outerHTML;
+  }
+  render() {
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: this.getTNList() }}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
