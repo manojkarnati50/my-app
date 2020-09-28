5f206be5b7e34002e50a6f1f7b12e1c797d94d79
Manoj Karnati <manoj.karnati@seygeninc.com>
2020-09-28 01:42:15 -0400

    divided jsx code into components

--- src/App.js
+++ src/App.js
@@ -11,6 +11,28 @@ class App extends Component {
     this.state = {
       screenName: "Voice Order Details",
       telephonesList: [],
+      serviceIdentifier: "",
+      ParentServiceIdentifier: "",
+      EquipmentId: "",
+      TelephoneNumberId: "",
+      WorkPlanItemIdentifier: "",
+      JobExists: "",
+      AccountStageCode: "",
+      Restore: "",
+      Action: "",
+      AccountId: "",
+      Status: "",
+      HierarchyLevel: "",
+      ProvisioningCode: "",
+      ProvisioningCodeDescription: "",
+      Type: "",
+      SubType: "",
+      StageCode: "",
+      Identifier: "",
+      Name: "",
+      Value: "",
+      customAction: "",
+      BeforeValue: "",
     };
     this.setTelePhoneHandler = this.setTelePhoneHandler.bind(this);
     this.getLeafItemNodes = this.getLeafItemNodes(this);
@@ -89,137 +111,149 @@ class App extends Component {
   };
 
   render() {
+    let logoDiv = (
+      <div class="box-1">
+        <img src={logo} alt="Logo" />
+      </div>
+    );
+    let telephonesListDiv = (
+      <div class="box-2">
+        <ul>
+          <span class="heading">TelephoneNumbers</span>
+          {this.state.telephonesList.map((item) => (
+            <li key={item}>
+              <p onClick={() => this.setTelePhoneHandler(item)}>{item}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+    let screenNameDiv = <div class="box-3">{this.state.screenName}</div>;
+    let serviceIdentifierTable = (
+      <table>
+        <tr>
+          <th>ServiceIdentifier</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>ParentServiceIdentifier</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>EquipmentId</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>TelephoneNumberId</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>WorkPlanItemIdentifier</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>TelephoneNumberICallingCardId UserDefinedIdentifier</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>AccountStageCode </th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>JobExists</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>Restore</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>Action</th>
+          <th>eightchar</th>
+        </tr>
+      </table>
+    );
+    let accountIdTable = (
+      <table>
+        <tr>
+          <th>AccountId</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>Status</th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th>HierarchyLevel</th>
+          <th>ROOT</th>
+        </tr>
+        <tr>
+          <th>ProvisioningCode</th>
+          <th>LINE</th>
+        </tr>
+        <tr colspan="2">
+          <th>ProvisioningCodeDescription</th>
+          <th></th>
+          <th>eightchar</th>
+        </tr>
+        <tr>
+          <th rowspan="4">LineOfBusines</th>
+
+          <tr>
+            <th>Type</th>
+            <th>T</th>
+          </tr>
+          <tr>
+            <th>SubType</th>
+            <th>LOCAL</th>
+          </tr>
+          <tr>
+            <th>StageCode</th>
+            <th>C</th>
+          </tr>
+        </tr>
+      </table>
+    );
+    let customfieldsTable = (
+      <table>
+        <tr rowspan="5">
+          <th>Custom Fields</th>
+        </tr>
+        <tr>
+          <th>Identifier</th>
+          <th>Name</th>
+          <th>Value</th>
+          <th>Action</th>
+          <th>Before Value</th>
+        </tr>
+        <tr></tr>
+      </table>
+    );
     return (
       <div class="pallete">
         <div class="row-container">
           <div class="col-container">
-            <div class="box-1">
-              <img src={logo} alt="Logo" />
-            </div>
-            <div class="box-2">
-              <ul>
-                <span class="heading">TelephoneNumbers</span>
-                {this.state.telephonesList.map((item) => (
-                  <li key={item}>
-                    <p onClick={() => this.setTelePhoneHandler(item)}>{item}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            {logoDiv}
+            {telephonesListDiv}
           </div>
-          <div class="container-2">
+          <div class="col-container">
+            <div class="container-2">
+              <div class="col-container">
+                {screenNameDiv}
+                <div class="row-container">
+                  <div>{serviceIdentifierTable}</div>
+                  <div>{accountIdTable}</div>
+                </div>
+                <div>{customfieldsTable}</div>
+              </div>
+            </div>
             <div class="container-3">
               <div class="col-container">
-                <div class="box-3">{this.state.screenName}</div>
                 <div class="row-container">
-                  <div>
-                    <table>
-                      <tr>
-                        <th>ServiceIdentifier</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>ParentServiceIdentifier</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>EquipmentId</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>TelephoneNumberId</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>WorkPlanItemIdentifier</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>
-                          TelephoneNumberICallingCardId UserDefinedIdentifier
-                        </th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>AccountStageCode </th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>JobExists</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>Restore</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>Action</th>
-                        <th>eightchar</th>
-                      </tr>
-                    </table>
-                  </div>
-                  <div>
-                    <table>
-                      <tr>
-                        <th>AccountId</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>Status</th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th>HierarchyLevel</th>
-                        <th>ROOT</th>
-                      </tr>
-                      <tr>
-                        <th>ProvisioningCode</th>
-                        <th>LINE</th>
-                      </tr>
-                      <tr colspan="2">
-                        <th>ProvisioningCodeDescription</th>
-                        <th></th>
-                        <th>eightchar</th>
-                      </tr>
-                      <tr>
-                        <th rowspan="4">LineOfBusines</th>
-
-                        <tr>
-                          <th>Type</th>
-                          <th>T</th>
-                        </tr>
-                        <tr>
-                          <th>SubType</th>
-                          <th>LOCAL</th>
-                        </tr>
-                        <tr>
-                          <th>StageCode</th>
-                          <th>C</th>
-                        </tr>
-                      </tr>
-                    </table>
-                  </div>
+                  <div>{serviceIdentifierTable}</div>
+                  <div>{accountIdTable}</div>
                 </div>
-                <div>
-                  <table>
-                    <tr rowspan="5">
-                      <th>Custom Fields</th>
-                    </tr>
-                    <tr>
-                      <th>Identifier</th>
-                      <th>Name</th>
-                      <th>Value</th>
-                      <th>Action</th>
-                      <th>Before Value</th>
-                    </tr>
-                    <tr></tr>
-                  </table>
-                </div>
-                <div class="box-5">
-                  <VoiceOrderDetails
-                    selectedPhoneNumber={this.state.selectedPhoneNumber}
-                  />
-                </div>
+                <div>{customfieldsTable}</div>
               </div>
             </div>
           </div>
